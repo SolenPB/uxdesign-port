5c11c1dc641f0d553f720d8e6661194bf7026d9e
SolenPB <solene.paratre@gmail.com>
2024-07-24 15:22:06 +0200

    maj homepage

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -5,7 +5,7 @@ export default {
 </script>
 
 <template>
-	<p> Bienvenue sur mon site !</p>
+	<h1>Bienvenue dans mon monde !</h1>
 	<br>
 	<p>C'est ici que je vous amène à découvrir mon parcours
 		et les réalisations que j'ai effectuées.</p>
