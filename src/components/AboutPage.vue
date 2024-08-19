6b8a486f53afcabf64c601c13d6caaed2c5dc8f5
SolenPB <solene.paratre@gmail.com>
2024-08-19 12:46:35 +0200

    Rangement code

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -7,11 +7,8 @@ export default {
 <template>
     <h1>A propos</h1>
 	<p>Après un parcours dans les sciences humaines et dans le commerce, je me reconvertie dans l’insertion professionnelle en 2020.</p><br> 
-    
     <p>Passionnée par le numérique depuis très jeune, j’ai vu naître une appétence pour l’UX design, grâce à une rencontre faite lors de ma première expérience en tant que conseillère en insertion professionnelle.</p><br> 
-    
     <p>Depuis lors, j’ai eu l’idée qu’un jour je développerai des outils pour les demandeurs d’emploi, notamment grâce à des simulations d’environnements de métiers.</p><br> 
-    
     <p>J’ai d’abord fait une première formation dans le développement web, avant de m’inscrire sur un Bachelor UX/UI design, pour la rentrée 2024.</p>
 </template>
 
