6edca5502d40af8a0cfe4b261ff2b943702b4296
SolenPB <solene.paratre@gmail.com>
2024-08-19 03:00:53 +0200

    modif about

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -10,7 +10,7 @@ export default {
     <br>
     <p>Passionnée par le numérique depuis très jeune, j’ai vu naître une appétence pour l’UX design, grâce à une rencontre faite lors de ma première expérience en tant que conseillère en insertion professionnelle.</p> 
     <br>
-    <p>Depuis lors, j’ai eu l’idée qu’un jour je développerai des outils pour les demandeurs d’emploi, notamment grâce à des simulations d’environnements de métiers.</p>Depuis lors, j’ai eu l’idée qu’un jour je développerai des outils pour les demandeurs d’emploi, notamment grâce à des simulations d’environnements de métiers. 
+    <p>Depuis lors, j’ai eu l’idée qu’un jour je développerai des outils pour les demandeurs d’emploi, notamment grâce à des simulations d’environnements de métiers.</p> 
     <br>
     <p>J’ai d’abord fait une première formation dans le développement web, avant de m’inscrire sur un Bachelor UX/UI design, pour la rentrée 2024.</p>
 </template>
