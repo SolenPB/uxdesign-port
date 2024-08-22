4ac72471371e74c47d611cfa332c571daf258636
SolenPB <solene.paratre@gmail.com>
2024-08-22 15:33:03 +0200

    maj home

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -6,13 +6,8 @@ export default {
 
 <template>
 	<h1>Bienvenue !</h1>
-	<p>Je suis Solène PARATRE--BADOIS, passionnée de numérique, d'ergonomie et d'apprentissage.</p>
-	<br>
-	<p>Curieuse avec un sens du service, j'aime chercher le pourquoi du comment.
-		Pour compléter mon CV, j'ai pensé qu'un site était nécessaire.  
-		Vous pouvez explorer la rubrique "A propos" pour des informations sur mon parcours,
-		ainsi que la section "Portofolio" pour mes réalisations. 
-	</p>
+	<p>Je suis Solène PARATRE--BADOIS, apprentie UI/UX designer, passionnée par la création d'expérience utilisateur intuitif et accessible.</p><br>
+	<p></p>
 </template>
 
 <style>
